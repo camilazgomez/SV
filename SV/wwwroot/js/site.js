58815e33408bef988c73bcf7a5b7b9e13ecddfa4
cppizarro <cppizarro@miuandes.cl>
2023-03-26 17:44:46 -0300

    feat: dinamic change in form

--- SV/wwwroot/js/site.js
+++ SV/wwwroot/js/site.js
@@ -2,3 +2,22 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+const addSellerBtn = document.querySelector("#add-seller");
+addSellerBtn.addEventListener("click", addSellerFormFields);
+
+function addSellerFormFields() {
+    const sellersDiv = document.querySelector(".sellers");
+    const newSellerRow = document.querySelector(".seller").cloneNode(true);
+    newSellerRow.removeAttribute("hidden");
+    sellersDiv.appendChild(newSellerRow);
+}
+
+const addHeirBtn = document.querySelector("#add-heir");
+addHeirBtn.addEventListener("click", addHeirFormFields)
+
+function addHeirFormFields() {
+    const heirsDiv = document.querySelector(".heirs");
+    const newHeirRow = document.querySelector(".heir").cloneNode(true);
+    newHeirRow.removeAttribute("hidden");
+    heirsDiv.appendChild(newHeirRow);
+}
