c0ee36f6a0cd110c6b97aa379ee7baafec946856
cppizarro <cppizarro@miuandes.cl>
2023-03-26 23:21:00 -0300

    feat: check with different ids

--- SV/wwwroot/js/form.js
+++ SV/wwwroot/js/form.js
@@ -6,16 +6,28 @@ function addSellerFormFields() {
     const newSellerRow = document.querySelector(".seller").cloneNode(true);
     newSellerRow.removeAttribute("hidden");
     sellersDiv.appendChild(newSellerRow);
+
+    const allLastUncreditedOwnershipSellerInputs = document.querySelectorAll('[name="uncreditedOwnershipSeller"]');
+    const lastUncreditedOwnershipSellersCount = allLastUncreditedOwnershipSellerInputs.length;
+    const lastUncreditedOwnershipSeller = allLastUncreditedOwnershipSellerInputs[lastUncreditedOwnershipSellersCount - 1];
+    const newUncreditedOwnershipSellerId = "uncreditedOwnershipSeller-" + (lastUncreditedOwnershipSellersCount - 1).toString();
+    lastUncreditedOwnershipSeller.setAttribute("id", newUncreditedOwnershipSellerId);
 }
 
 const addBuyerBtn = document.querySelector("#add-buyer");
-addBuyerBtn.addEventListener("click", addBuyerFormFields)
+addBuyerBtn.addEventListener("click", addBuyerFormFields);
 
 function addBuyerFormFields() {
     const heirsDiv = document.querySelector(".buyers");
     const newHeirRow = document.querySelector(".buyer").cloneNode(true);
     newHeirRow.removeAttribute("hidden");
     heirsDiv.appendChild(newHeirRow);
+
+    const allLastUncreditedOwnershipBuyersInputs = document.querySelectorAll('[name="uncreditedOwnershipBuyer"]');
+    const lastUncreditedOwnershipBuyersCount = allLastUncreditedOwnershipBuyersInputs.length;
+    const lastUncreditedOwnershipBuyer = allLastUncreditedOwnershipBuyersInputs[lastUncreditedOwnershipBuyersCount - 1];
+    const newUncreditedOwnershipBuyerId = "uncreditedOwnershipBuyer-" + (lastUncreditedOwnershipBuyersCount - 1).toString();
+    lastUncreditedOwnershipBuyer.setAttribute("id", newUncreditedOwnershipBuyerId);
 }
 
 window.onload = function () {
