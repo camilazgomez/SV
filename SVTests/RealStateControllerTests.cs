b4c7395186265b915f898b457c960a2fcf056144
cppizarro <cppizarro@miuandes.cl>
2023-06-23 21:09:31 -0400

    tests (43% coverage)

--- SVTests/RealStateControllerTests.cs
+++ SVTests/RealStateControllerTests.cs
@@ -4,26 +4,19 @@ using Moq;
 using SV.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Primitives;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
-using Microsoft.AspNetCore.Routing;
-using Microsoft.SqlServer.Server;
-using static Microsoft.AspNetCore.Mvc.ModelBinding.ModelStateDictionary;
-using System.Linq.Expressions;
-using Microsoft.CodeAnalysis;
-using Microsoft.AspNetCore.Mvc.ViewFeatures;
 
 namespace SVTests
 {
     [TestClass]
     public class RealStateControllerTests
     {
-        private RealStateFormsController _controller;
-        private Mock<InscripcionesBrDbContext> _mockDbContext;
+
+        private readonly Mock<InscripcionesBrDbContext> _mockDbContext = new();
+        private RealStateFormsController? _controller;
 
         [TestInitialize]
         public void Setup()
         {
-            _mockDbContext = new Mock<InscripcionesBrDbContext>();
             _controller = new RealStateFormsController(_mockDbContext.Object);
         }
 
@@ -49,7 +42,7 @@ namespace SVTests
                 { "uncreditedClickedBuyer", new StringValues(new string[] {"false", "false"}) }
             });
 
-            RealStateForm rsform = new RealStateForm
+            RealStateForm rsform = new()
             {
                 AttentionNumber = 12,
                 NatureOfTheDeed = "Compraventa",
@@ -75,53 +68,57 @@ namespace SVTests
                 rsform
             };
 
-            List<Person> people = new List<Person>()
+            List<Person> people = new()
             {
                 new Person { Forms = rsform, Rut = "10.534.906-8", OwnershipPercentage = 30, UncreditedOwnership = false, Seller = true, Heir = false  },
                 new Person { Forms = rsform, Rut = "20.427.455-K", OwnershipPercentage = 10, UncreditedOwnership = false, Seller = false, Heir = true  },
                 new Person { Forms = rsform, Rut = "20.428.706-6", OwnershipPercentage = 20, UncreditedOwnership = false, Seller = false, Heir = true  }
             };
 
-            List<MultiOwner> multiOwner = new List<MultiOwner>()
+            List<MultiOwner> multiOwner = new()
             {
-                new MultiOwner { Rut = "10.534.906-8", OwnershipPercentage = 30, Commune = "Las Condes", Block = "32", Property = "123", Sheets = 1, 
+                new MultiOwner { Rut = "10.534.906-8", OwnershipPercentage = 30, Commune = "Las Condes", Block = "32", Property = "123", Sheets = 1,
                     InscriptionDate = DateTime.Parse("2021-06-22T19:15"), InscriptionNumber = 1, ValidityYearBegin = 2021, ValidityYearFinish = null }
             };
 
-            _controller.ControllerContext = new ControllerContext
+            if (_controller != null)
             {
-                HttpContext = new DefaultHttpContext { Request = { Form = form } }
-            };
+                _controller.ControllerContext = new ControllerContext
+                {
+                    HttpContext = new DefaultHttpContext { Request = { Form = form } }
+                };
 
-            var _mockDbSetRSF = new Mock<DbSet<RealStateForm>>();
-            _mockDbSetRSF.As<IQueryable<RealStateForm>>().Setup(m => m.Provider).Returns(rsforms.AsQueryable().Provider);
-            _mockDbSetRSF.As<IQueryable<RealStateForm>>().Setup(m => m.Expression).Returns(rsforms.AsQueryable().Expression);
-            _mockDbSetRSF.As<IQueryable<RealStateForm>>().Setup(m => m.ElementType).Returns(rsforms.AsQueryable().ElementType);
-            _mockDbSetRSF.As<IQueryable<RealStateForm>>().Setup(m => m.GetEnumerator()).Returns(() => rsforms.AsQueryable().GetEnumerator());
 
-            var _mockDbSetP = new Mock<DbSet<Person>>();
-            _mockDbSetP.As<IQueryable<Person>>().Setup(m => m.Provider).Returns(people.AsQueryable().Provider);
-            _mockDbSetP.As<IQueryable<Person>>().Setup(m => m.Expression).Returns(people.AsQueryable().Expression);
-            _mockDbSetP.As<IQueryable<Person>>().Setup(m => m.ElementType).Returns(people.AsQueryable().ElementType);
-            _mockDbSetP.As<IQueryable<Person>>().Setup(m => m.GetEnumerator()).Returns(() => people.AsQueryable().GetEnumerator());
+                var _mockDbSetRSF = new Mock<DbSet<RealStateForm>>();
+                _mockDbSetRSF.As<IQueryable<RealStateForm>>().Setup(m => m.Provider).Returns(rsforms.AsQueryable().Provider);
+                _mockDbSetRSF.As<IQueryable<RealStateForm>>().Setup(m => m.Expression).Returns(rsforms.AsQueryable().Expression);
+                _mockDbSetRSF.As<IQueryable<RealStateForm>>().Setup(m => m.ElementType).Returns(rsforms.AsQueryable().ElementType);
+                _mockDbSetRSF.As<IQueryable<RealStateForm>>().Setup(m => m.GetEnumerator()).Returns(() => rsforms.AsQueryable().GetEnumerator());
 
-            var _mockDbSetMO = new Mock<DbSet<MultiOwner>>();
-            _mockDbSetMO.As<IQueryable<MultiOwner>>().Setup(m => m.Provider).Returns(multiOwner.AsQueryable().Provider);
-            _mockDbSetMO.As<IQueryable<MultiOwner>>().Setup(m => m.Expression).Returns(multiOwner.AsQueryable().Expression);
-            _mockDbSetMO.As<IQueryable<MultiOwner>>().Setup(m => m.ElementType).Returns(multiOwner.AsQueryable().ElementType);
-            _mockDbSetMO.As<IQueryable<MultiOwner>>().Setup(m => m.GetEnumerator()).Returns(() => multiOwner.AsQueryable().GetEnumerator());
+                var _mockDbSetP = new Mock<DbSet<Person>>();
+                _mockDbSetP.As<IQueryable<Person>>().Setup(m => m.Provider).Returns(people.AsQueryable().Provider);
+                _mockDbSetP.As<IQueryable<Person>>().Setup(m => m.Expression).Returns(people.AsQueryable().Expression);
+                _mockDbSetP.As<IQueryable<Person>>().Setup(m => m.ElementType).Returns(people.AsQueryable().ElementType);
+                _mockDbSetP.As<IQueryable<Person>>().Setup(m => m.GetEnumerator()).Returns(() => people.AsQueryable().GetEnumerator());
 
-            _mockDbContext.Setup(c => c.RealStateForms).Returns(_mockDbSetRSF.Object);
-            _mockDbContext.Setup(c => c.People).Returns(_mockDbSetP.Object);
-            _mockDbContext.Setup(c => c.MultiOwners).Returns(_mockDbSetMO.Object);
+                var _mockDbSetMO = new Mock<DbSet<MultiOwner>>();
+                _mockDbSetMO.As<IQueryable<MultiOwner>>().Setup(m => m.Provider).Returns(multiOwner.AsQueryable().Provider);
+                _mockDbSetMO.As<IQueryable<MultiOwner>>().Setup(m => m.Expression).Returns(multiOwner.AsQueryable().Expression);
+                _mockDbSetMO.As<IQueryable<MultiOwner>>().Setup(m => m.ElementType).Returns(multiOwner.AsQueryable().ElementType);
+                _mockDbSetMO.As<IQueryable<MultiOwner>>().Setup(m => m.GetEnumerator()).Returns(() => multiOwner.AsQueryable().GetEnumerator());
 
-            var result = await _controller.Create(rsform);
+                _mockDbContext.Setup(c => c.RealStateForms).Returns(_mockDbSetRSF.Object);
+                _mockDbContext.Setup(c => c.People).Returns(_mockDbSetP.Object);
+                _mockDbContext.Setup(c => c.MultiOwners).Returns(_mockDbSetMO.Object);
 
-            var redirectResult = result as RedirectToActionResult;
-            Assert.IsNotNull(redirectResult);
-            Assert.AreEqual("Index", redirectResult.ActionName);
+                var result = await _controller.Create(rsform);
 
-            _mockDbContext.Verify(c => c.Add(It.IsAny<RealStateForm>()), Times.Once);
+                var redirectResult = result as RedirectToActionResult;
+                Assert.IsNotNull(redirectResult);
+                Assert.AreEqual("Index", redirectResult.ActionName);
+
+                _mockDbContext.Verify(c => c.Add(It.IsAny<RealStateForm>()), Times.Once);
+            }
         }
 
         [TestMethod]
@@ -137,7 +134,7 @@ namespace SVTests
                 { "Sheets", new StringValues("2") },
                 { "InscriptionDate", new StringValues("2022-06-22T19:15") },
                 { "InscriptionNumber", new StringValues("2") },
-                { "rutSeller", new StringValues(new string[] {}) },
+                { "rutSeller", new StringValues(Array.Empty<string>()) },
                 { "ownershipPercentageSeller", new StringValues(new string[] {"30"}) },
                 { "uncreditedClickedSeller", new StringValues(new string[] {"false"}) },
                 { "rutBuyer", new StringValues(new string[] { "20.427.455-K", "20.428.706-6" }) },
@@ -145,7 +142,7 @@ namespace SVTests
                 { "uncreditedClickedBuyer", new StringValues(new string[] {"false", "false"}) }
             });
 
-            RealStateForm rsform = new RealStateForm
+            RealStateForm rsform = new()
             {
                 AttentionNumber = 12,
                 NatureOfTheDeed = "Compraventa",
@@ -171,63 +168,65 @@ namespace SVTests
                 rsform
             };
 
-            List<Person> people = new List<Person>()
+            List<Person> people = new()
             {
                 new Person { Forms = rsform, Rut = "20.427.455-K", OwnershipPercentage = 10, UncreditedOwnership = false, Seller = false, Heir = true  },
                 new Person { Forms = rsform, Rut = "20.428.706-6", OwnershipPercentage = 20, UncreditedOwnership = false, Seller = false, Heir = true  }
             };
 
-            List<MultiOwner> multiOwner = new List<MultiOwner>()
+            List<MultiOwner> multiOwner = new()
             {
                 new MultiOwner { Rut = "10.534.906-8", OwnershipPercentage = 30, Commune = "Las Condes", Block = "32", Property = "123", Sheets = 1,
                     InscriptionDate = DateTime.Parse("2021-06-22T19:15"), InscriptionNumber = 1, ValidityYearBegin = 2021, ValidityYearFinish = null }
             };
 
-            List<Commune> communes = new List<Commune>()
+            List<Commune> communes = new()
             {
                 new Commune { Name = "Algarrobo" }
             };
 
-            _controller.ControllerContext = new ControllerContext
+            if (_controller != null)
             {
-                HttpContext = new DefaultHttpContext { Request = { Form = form } }
-            };
+                _controller.ControllerContext = new ControllerContext
+                {
+                    HttpContext = new DefaultHttpContext { Request = { Form = form } }
+                };
 
-            var _mockDbSetRSF = new Mock<DbSet<RealStateForm>>();
-            _mockDbSetRSF.As<IQueryable<RealStateForm>>().Setup(m => m.Provider).Returns(rsforms.AsQueryable().Provider);
-            _mockDbSetRSF.As<IQueryable<RealStateForm>>().Setup(m => m.Expression).Returns(rsforms.AsQueryable().Expression);
-            _mockDbSetRSF.As<IQueryable<RealStateForm>>().Setup(m => m.ElementType).Returns(rsforms.AsQueryable().ElementType);
-            _mockDbSetRSF.As<IQueryable<RealStateForm>>().Setup(m => m.GetEnumerator()).Returns(() => rsforms.AsQueryable().GetEnumerator());
+                var _mockDbSetRSF = new Mock<DbSet<RealStateForm>>();
+                _mockDbSetRSF.As<IQueryable<RealStateForm>>().Setup(m => m.Provider).Returns(rsforms.AsQueryable().Provider);
+                _mockDbSetRSF.As<IQueryable<RealStateForm>>().Setup(m => m.Expression).Returns(rsforms.AsQueryable().Expression);
+                _mockDbSetRSF.As<IQueryable<RealStateForm>>().Setup(m => m.ElementType).Returns(rsforms.AsQueryable().ElementType);
+                _mockDbSetRSF.As<IQueryable<RealStateForm>>().Setup(m => m.GetEnumerator()).Returns(() => rsforms.AsQueryable().GetEnumerator());
 
-            var _mockDbSetP = new Mock<DbSet<Person>>();
-            _mockDbSetP.As<IQueryable<Person>>().Setup(m => m.Provider).Returns(people.AsQueryable().Provider);
-            _mockDbSetP.As<IQueryable<Person>>().Setup(m => m.Expression).Returns(people.AsQueryable().Expression);
-            _mockDbSetP.As<IQueryable<Person>>().Setup(m => m.ElementType).Returns(people.AsQueryable().ElementType);
-            _mockDbSetP.As<IQueryable<Person>>().Setup(m => m.GetEnumerator()).Returns(() => people.AsQueryable().GetEnumerator());
+                var _mockDbSetP = new Mock<DbSet<Person>>();
+                _mockDbSetP.As<IQueryable<Person>>().Setup(m => m.Provider).Returns(people.AsQueryable().Provider);
+                _mockDbSetP.As<IQueryable<Person>>().Setup(m => m.Expression).Returns(people.AsQueryable().Expression);
+                _mockDbSetP.As<IQueryable<Person>>().Setup(m => m.ElementType).Returns(people.AsQueryable().ElementType);
+                _mockDbSetP.As<IQueryable<Person>>().Setup(m => m.GetEnumerator()).Returns(() => people.AsQueryable().GetEnumerator());
 
-            var _mockDbSetMO = new Mock<DbSet<MultiOwner>>();
-            _mockDbSetMO.As<IQueryable<MultiOwner>>().Setup(m => m.Provider).Returns(multiOwner.AsQueryable().Provider);
-            _mockDbSetMO.As<IQueryable<MultiOwner>>().Setup(m => m.Expression).Returns(multiOwner.AsQueryable().Expression);
-            _mockDbSetMO.As<IQueryable<MultiOwner>>().Setup(m => m.ElementType).Returns(multiOwner.AsQueryable().ElementType);
-            _mockDbSetMO.As<IQueryable<MultiOwner>>().Setup(m => m.GetEnumerator()).Returns(() => multiOwner.AsQueryable().GetEnumerator());
+                var _mockDbSetMO = new Mock<DbSet<MultiOwner>>();
+                _mockDbSetMO.As<IQueryable<MultiOwner>>().Setup(m => m.Provider).Returns(multiOwner.AsQueryable().Provider);
+                _mockDbSetMO.As<IQueryable<MultiOwner>>().Setup(m => m.Expression).Returns(multiOwner.AsQueryable().Expression);
+                _mockDbSetMO.As<IQueryable<MultiOwner>>().Setup(m => m.ElementType).Returns(multiOwner.AsQueryable().ElementType);
+                _mockDbSetMO.As<IQueryable<MultiOwner>>().Setup(m => m.GetEnumerator()).Returns(() => multiOwner.AsQueryable().GetEnumerator());
 
-            var _mockDbSetC = new Mock<DbSet<Commune>>();
-            _mockDbSetC.As<IQueryable<Commune>>().Setup(m => m.Provider).Returns(communes.AsQueryable().Provider);
-            _mockDbSetC.As<IQueryable<Commune>>().Setup(m => m.Expression).Returns(communes.AsQueryable().Expression);
-            _mockDbSetC.As<IQueryable<Commune>>().Setup(m => m.ElementType).Returns(communes.AsQueryable().ElementType);
-            _mockDbSetC.As<IQueryable<Commune>>().Setup(m => m.GetEnumerator()).Returns(() => communes.AsQueryable().GetEnumerator());
+                var _mockDbSetC = new Mock<DbSet<Commune>>();
+                _mockDbSetC.As<IQueryable<Commune>>().Setup(m => m.Provider).Returns(communes.AsQueryable().Provider);
+                _mockDbSetC.As<IQueryable<Commune>>().Setup(m => m.Expression).Returns(communes.AsQueryable().Expression);
+                _mockDbSetC.As<IQueryable<Commune>>().Setup(m => m.ElementType).Returns(communes.AsQueryable().ElementType);
+                _mockDbSetC.As<IQueryable<Commune>>().Setup(m => m.GetEnumerator()).Returns(() => communes.AsQueryable().GetEnumerator());
 
-            _mockDbContext.Setup(c => c.RealStateForms).Returns(_mockDbSetRSF.Object);
-            _mockDbContext.Setup(c => c.People).Returns(_mockDbSetP.Object);
-            _mockDbContext.Setup(c => c.MultiOwners).Returns(_mockDbSetMO.Object);
-            _mockDbContext.Setup(c => c.Commune).Returns(_mockDbSetC.Object);
+                _mockDbContext.Setup(c => c.RealStateForms).Returns(_mockDbSetRSF.Object);
+                _mockDbContext.Setup(c => c.People).Returns(_mockDbSetP.Object);
+                _mockDbContext.Setup(c => c.MultiOwners).Returns(_mockDbSetMO.Object);
+                _mockDbContext.Setup(c => c.Commune).Returns(_mockDbSetC.Object);
 
-            var result = await _controller.Create(rsform);
+                var result = await _controller.Create(rsform);
 
-            var viewResult = result as ViewResult;
-            Assert.IsNotNull(viewResult);
-            Assert.IsInstanceOfType(viewResult.Model, typeof(RealStateForm));
-
+                var viewResult = result as ViewResult;
+                Assert.IsNotNull(viewResult);
+                Assert.IsInstanceOfType(viewResult.Model, typeof(RealStateForm));
+            }
         }
 
         [TestMethod]
@@ -248,13 +247,16 @@ namespace SVTests
 
             _mockDbContext.Setup(c => c.RealStateForms).Returns(_mockDbSetRSF.Object);
 
-            var result = await _controller.Index();
+            if (_controller != null)
+            {
+                var result = await _controller.Index();
 
-            var viewResult = result as ViewResult;
-            Assert.IsNotNull(viewResult);
-            Assert.IsNotNull(viewResult.Model);
-            var model = viewResult.Model as List<RealStateForm>;
-            CollectionAssert.AreEqual(rsforms, model);
+                var viewResult = result as ViewResult;
+                Assert.IsNotNull(viewResult);
+                Assert.IsNotNull(viewResult.Model);
+                var model = viewResult.Model as List<RealStateForm>;
+                CollectionAssert.AreEqual(rsforms, model);
+            }
         }
 
         public class TestAsyncEnumerator<T> : IAsyncEnumerator<T>
@@ -276,6 +278,7 @@ namespace SVTests
             public ValueTask DisposeAsync()
             {
                 _enumerator.Dispose();
+                GC.SuppressFinalize(this);
                 return new ValueTask();
             }
         }
